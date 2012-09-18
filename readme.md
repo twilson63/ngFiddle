5947f01070cf22566eb0f9e95bd260528d848701
Tom Wilson <tom@jackhq.com>
2012-09-18 19:12:39 -0400

    update couch url

---
+++ readme.md
@@ -0,0 +1,14 @@
+# ngFiddle
+
+A jsfiddle clone using angular, nodejs, and couchdb
+
+## Usage
+
+Install NodeJs
+Clone Repo
+
+``` sh
+cd ngFiddle
+npm install
+node server.js
+```
--- server.js
+++ server.js
@@ -5,24 +5,25 @@ var http = require('http'),
   hogan = require('hogan.js'),
   template = hogan.compile(fs.readFileSync('./template.html.mu', 'utf-8'));
 
+var COUCH = process.env.COUCH || 'http://localhost:5984';
+
 http.createServer(function(req,res){
   var pathname = req.url;
   if (req.url === '/') { pathname = '/index.html' }
   if (pathname === '/fiddle' && req.method === 'POST') {
-    req.pipe(request('http://localhost:5984/fiddle')).pipe(res);
+    req.pipe(request(COUCH + pathname)).pipe(res);
   } else if (/^\/fiddle\//.test(pathname) && req.method === 'PUT') {
-    req.pipe(request.put('http://localhost:5984' + pathname)).pipe(res);
+    req.pipe(request.put(COUCH + pathname)).pipe(res);
   } else if (/^\/fiddle\//.test(pathname) && req.method === 'GET') {
     // merge html css and js into doc and return.
-    request('http://localhost:5984' + pathname, {json: true}, function(e,r,b){
+    request(COUCH + pathname, {json: true}, function(e,r,b){
       if(e) { 
         res.writeHead(500, {'Content-Type': 'text/plain'});
         res.end(e.message);
         return;
       }
-      var foo = template.render(b);
       res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(foo);
+      res.end(template.render(b));
     });
   } else {
     filed(__dirname + pathname).pipe(res);

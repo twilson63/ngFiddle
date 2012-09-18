54f51d0b93d13ec4d0263b7f1899d41756b39c57
Tom Wilson <tom@jackhq.com>
2012-09-18 19:19:35 -0400

    fixed bug with loading iframe

--- scripts/app.js
+++ scripts/app.js
@@ -3,17 +3,17 @@ function fiddleCtrl($scope, $http) {
   $scope.css = "h1 {\r\n  color: blue;\r\n}";
   $scope.js = "function helloCtrl($scope) {\r\n  $scope.world = \"world\";\r\n};"
   $scope.preview = function() {
-    $scope.page = 'http://localhost:3000/loading.html';
+    $scope.page = '/loading.html';
     //$scope.page = 'http://localhost:3000/';
     if ($scope._id) {
       $http.put('/fiddle/' + $scope._id, { html: $scope.html, css: $scope.css, js: $scope.js, _rev: $scope._rev }).success(function(data){
-        $scope.page = 'http://localhost:3000/fiddle/' + data.id;
+        $scope.page = '/fiddle/' + data.id;
         $scope._id = data.id;
         $scope._rev = data.rev;
       });
     } else {
       $http.post('/fiddle', { html: $scope.html, css: $scope.css, js: $scope.js }).success(function(data){
-        $scope.page = 'http://localhost:3000/fiddle/' + data.id;
+        $scope.page = '/fiddle/' + data.id;
         $scope._id = data.id;
         $scope._rev = data.rev;
       });
